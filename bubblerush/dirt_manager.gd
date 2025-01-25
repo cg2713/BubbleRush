5772faced3d898e246134efb4cb8e6a0e4d8f1e6
Jairo Honorio <registrofx@gmail.com>
2025-01-25 12:51:00 -0800

    Add dirt manager and slowly respawn dirt

---
+++ bubblerush/dirt_manager.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var timer = Timer.new()
+	add_child(timer)
+	timer.timeout.connect(reduce_children_transparency)
+	timer.one_shot = false
+	timer.wait_time = 0.1
+	timer.start()
+
+func reduce_children_transparency():
+	for child in get_children():
+		if child is Node3D:
+			var t = child.get_node("MeshInstance3D").transparency
+			t -= 0.01
+			child.get_node("MeshInstance3D").transparency = max(t, 0)
