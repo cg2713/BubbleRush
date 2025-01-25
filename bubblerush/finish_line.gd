34b573010ae97a74c90274d8acc9adb529dc3645
Alex Benton <alexpbenton@gmail.com>
2025-01-24 21:09:47 -0800

    Started Finish Line
    IN PROGRESS

---
+++ bubblerush/finish_line.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is car:
+		print("You win!")
+
+
+func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body is car:
+		print("You win!")
