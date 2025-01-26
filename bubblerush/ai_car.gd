dc877d38624f9bf473ec16fecf7153070f596071
Jairo Honorio <registrofx@gmail.com>
2025-01-25 17:23:03 -0800

    Add randomness to ai driving, making them realistic

--- bubblerush/ai_car.gd
+++ bubblerush/ai_car.gd
@@ -4,24 +4,32 @@ extends car
 @export var look_ahead_distance = 4
 
 var current_waypoint : int = 0
+var objective : Vector3
 
 func get_input_dir():
 	print("curr ", current_waypoint)
-	var target_waypoint : Vector3 = waypoint_path.curve.get_point_position(current_waypoint)
+	if objective == Vector3.ZERO:
+		var target_waypoint : Vector3 = waypoint_path.curve.get_point_position(current_waypoint)
+		objective = target_waypoint
+		# Randomize objective a little bit so that different AI cars dont clutter
+		const randomness = 4
+		objective.x += randf_range(-randomness,randomness)
+		objective.z += randf_range(-randomness,randomness)
 	
 	# Find the direction to the waypoint in the car's local space
-	var local_target_direction = global_transform.basis.inverse() * (target_waypoint - global_transform.origin).normalized()
+	var local_target_direction = global_transform.basis.inverse() * (objective - global_transform.origin).normalized()
 
 	#print("dir ", local_target_direction)
 	#print("distance ", (target_waypoint - global_transform.origin).length())
 
 	# Are we there yet?
-	if (target_waypoint - global_transform.origin).length() < look_ahead_distance:
+	if (objective - global_transform.origin).length() < look_ahead_distance:
 		# Target the next waypoint
 		current_waypoint = (current_waypoint + 1) % waypoint_path.curve.point_count
+		objective = Vector3.ZERO
 
 	$debugMarkerSteer.position = local_target_direction * 2 # Visual debug in local space
-	$debugMarkerObjective.global_position = target_waypoint
+	$debugMarkerObjective.global_position = objective
 
 	# Calculate the steering input based on the local direction:
 	var steer_input = local_target_direction.x # Positive if target is to the right, negative if to the left
