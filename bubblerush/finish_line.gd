39373547fe50505313fcdd598231e07ae697d504
Xelalego <alexpbenton@gmail.com>
2025-01-27 14:40:55 -0800

    Finish line SFX

--- bubblerush/finish_line.gd
+++ bubblerush/finish_line.gd
@@ -2,6 +2,7 @@ extends Node3D
 class_name Finish_Line
 
 @export var value: int = 1
+@export var SFX : Array[AudioStream]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,6 +19,9 @@ func _on_body_entered(body: Node3D) -> void:
 	if body is car:
 		if len(body.passedCheckpoints) >= len(get_tree().current_scene.Checkpoints):
 			body._complete_lap()
+			var playback = $AudioStreamPlayer3D.get_stream_playback()
+			for sfx in SFX:
+				playback.play_stream(sfx, 0, 1, 1, 0, "Master");
 	pass # Replace with function body.
 
 #func _finished():
